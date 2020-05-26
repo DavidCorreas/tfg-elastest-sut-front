ffdd4faf051f89c5c2bb5e66748821456b521743
DavidCorreas <davidestu77@gmail.com>
2020-05-26 19:39:50 +0200

    81 Working with the file URL

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -23,7 +23,8 @@ export class PostsService {
           return {
             title: post.title,
             content: post.content,
-            id: post._id
+            id: post._id,
+            imagePath: post.imagePath
           }
         })
       }))
@@ -38,19 +39,23 @@ export class PostsService {
   }
 
   getPost(postId: string) {
-    return this.http.get<{ _id: string, title: string, content: string }>('http://localhost:3000/api/posts/' + postId);
+    return this.http
+      .get<{ _id: string, title: string, content: string }>('http://localhost:3000/api/posts/' + postId);
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = {
-      id: null,
-      title: title,
-      content: content
-    };
-    this.http.post<{postId: string, message: string}>('http://localhost:3000/api/posts', post)
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title)
+    postData.append('content', content)
+    postData.append('image', image, title);
+    this.http.post<{post: Post, message: string}>('http://localhost:3000/api/posts', postData)
       .subscribe((responseData) => {
-        console.log(responseData.message);
-        post.id = responseData.postId;
+        const post: Post = {
+          id: responseData.post.id,
+          title: title,
+          content: content,
+          imagePath: responseData.post.imagePath
+        }
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
@@ -61,7 +66,8 @@ export class PostsService {
     const post: Post = {
       id: postId,
       title: title,
-      content: content
+      content: content,
+      imagePath: null
     };
     this.http.put<{ message: string}>('http://localhost:3000/api/posts/' + postId, post)
       .subscribe( response => {
